5dee66dae4e45c4c90e206da6dc19481dd939500
i2god1314 <i2god1314@163.com>
2023-04-06 12:57:17 +0900

    システムが正常に機能しなくなる、復旧不能なエラーでない限り、panicを使用することは推奨されません。

--- error/errorResErr.go
+++ error/errorResErr.go
@@ -14,6 +14,15 @@ func div(a, b int) (res int, err error) {
 	return
 }
 
+func div2(a, b int) (res int) {
+	if b == 0 {
+		panic("除数はゼロにすることはできません。")
+	} else {
+		res = a / b
+	}
+	return
+}
+
 func main() {
 	//res, err := div(10, 5)
 	res, err := div(10, 0)
@@ -22,4 +31,13 @@ func main() {
 	} else {
 		fmt.Println(res)
 	}
+	res = div2(10, 0)
+	fmt.Println(res)
+
+	var arr = [3]int{1, 3, 4}
+	arr[5] = 666 // エラー発生:invalid argument: index 5 out of bounds [0:3]
+	fmt.Println(arr)
+
+	var res2 = 10 / 0 // エラー発生:invalid operation: division by zero
+	fmt.Println(res2)
 }
