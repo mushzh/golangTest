325ea75292ad43e919b0013516274292763e5b19
i2god1314 <i2god1314@163.com>
2023-04-25 20:27:25 +0900

    们可以通过ok-idiom模式判断管道是否关闭, 如果关闭会返回false Channel阻塞现象 单独在主线程中操作管道, 写满了会报错, 没有数据去获取也会报错 只要在协程中操作管道过, 写满了就会阻塞, 没有就数据去获取也会阻塞

--- main/testForChannel.go
+++ main/testForChannel.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("长度是", len(mych), "容量是", cap(mych)) // 长度是 1 容量是 3
 	// 5.取出管道中写入的数据
 	num := <-mych
-	fmt.Println("num = ", num)                            // num =  666
+	fmt.Println("num = ", num)                      // num =  666
 	fmt.Println("长度是", len(mych), "容量是", cap(mych)) // 长度是 0 容量是 3
 
 	// 注意点: 管道中只能存放声明的数据类型, 不能存放其它数据类型
@@ -27,5 +27,32 @@ func main() {
 	mych <- 666
 	mych <- 777
 	mych <- 888
-	mych <- 999
+	//mych <- 999
+
+	//for i := 0; i < len(mych); i++ {
+	//	fmt.Println(<-mych) // 输出结果不正确
+	//}
+
+	// 3.写入完数据之后先关闭管道
+	// 注意点: 管道关闭之后只能读不能写
+	close(mych)
+	//mych <- 999 // 报错
+
+	// 4.遍历管道
+	// 利用for range遍历, 必须先关闭管道, 否则会报错
+	//for value := range mych {
+	//	fmt.Println(value)
+	//}
+
+	// close主要用途:
+	// 在企业开发中我们可能不确定管道有还没有有数据, 所以我们可能一直获取
+	// 但是我们可以通过ok-idiom模式判断管道是否关闭, 如果关闭会返回false给ok
+	for {
+		if num, ok := <-mych; ok {
+			fmt.Println(num)
+		} else {
+			break
+		}
+	}
+	fmt.Println("数据读取完毕")
 }
